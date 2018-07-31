3e20d18ffb387fe4b6acec8d45d51ef957dcbb1e
jensend <jensend@anl.gov>
2018-07-31 15:17:46 -0500

    cleaning up, removing gitlab deps, renaming all the things

---
+++ README.md
@@ -0,0 +1,3 @@
+# SQLios
+
+Project to parse Nagios performance and event data, and push it to an SQL database like PostgreSQL/TimescaleDB
---
+++ nagios/structs.go
@@ -0,0 +1,11 @@
+package nagios
+
+// Block is made up of the section name from status.dat, lines of the block,
+// Created time of the current status.dat file based on the info section
+// and LastCreated time which is the created time from the last read status.dat
+type Block struct {
+	Name        string
+	Lines       []string
+	Created     int64
+	LastCreated int64
+}
